4fa2c84314bfef32726738ea119257c730970101
jpomykala <evelan3094@gmail.com>
2019-04-20 18:32:37 +0200

    Add test coverage

--- src/test/java/io/simplelocalize/cli/configuration/ConfigurationLoaderTest.java
+++ src/test/java/io/simplelocalize/cli/configuration/ConfigurationLoaderTest.java
@@ -22,7 +22,7 @@ public class ConfigurationLoaderTest {
     Assertions.assertThat(configuration.getClientSecret()).isEqualTo("7890");
     Assertions.assertThat(configuration.getProjectToken()).isEqualTo("abc");
     Assertions.assertThat(configuration.getProjectType()).isEqualTo("react-intl");
-    Assertions.assertThat(configuration.getSearchDir()).isEqualTo("/Users/jpomykala/Workspace/");
+    Assertions.assertThat(configuration.getSearchDir()).isEqualTo("./target/test-classes/react-intl");
   }
 
   @Test
