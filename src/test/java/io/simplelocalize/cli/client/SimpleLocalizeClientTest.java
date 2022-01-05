f933de45ea2dfd3623bcaf81152bfe84e962396d
Jakub Pomykała <jakub.pomykala@gmail.com>
2022-01-05 18:07:32 +0100

    Adjust

--- src/test/java/io/simplelocalize/cli/client/SimpleLocalizeClientTest.java
+++ src/test/java/io/simplelocalize/cli/client/SimpleLocalizeClientTest.java
@@ -73,7 +73,7 @@ public class SimpleLocalizeClientTest
                             .withMethod("POST")
                             .withPath("/cli/v1/upload")
                             .withQueryStringParameter("uploadFormat", "multi-language-json")
-                            .withQueryStringParameter("importOptions", "MULTI_FILE")
+                            .withQueryStringParameter("uploadOptions", "MULTI_FILE")
                             .withQueryStringParameter("projectPath", "./my-path/my-file/test.json")
                             .withHeader("X-SimpleLocalize-Token", "81707741b64e68427e1a2c20e75095b1"),
                     Times.exactly(1))
