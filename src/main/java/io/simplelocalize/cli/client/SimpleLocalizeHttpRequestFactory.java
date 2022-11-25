8579ebf0dba9774de6ed84dc09b2c26fed281844
Jakub Pomykała <jakub.pomykala@gmail.com>
2022-11-25 14:04:00 +0100

    Remove SecureRandom

--- src/main/java/io/simplelocalize/cli/client/SimpleLocalizeHttpRequestFactory.java
+++ src/main/java/io/simplelocalize/cli/client/SimpleLocalizeHttpRequestFactory.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.nio.file.Path;
-import java.security.SecureRandom;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,13 +18,10 @@ public class SimpleLocalizeHttpRequestFactory
   private static final String CLI_VERSION_HEADER_NAME = "X-SimpleLocalize-Cli-Version";
   private static final String CONTENT_TYPE_HEADER_NAME = "Content-Type";
   private final String apiKey;
-  private final SecureRandom random;
-
 
   public SimpleLocalizeHttpRequestFactory(String apiKey)
   {
     this.apiKey = apiKey;
-    this.random = new SecureRandom();
   }
 
   HttpRequest createSendKeysRequest(URI uri, Collection<String> keys) throws JsonProcessingException
@@ -38,7 +34,7 @@ public class SimpleLocalizeHttpRequestFactory
 
   HttpRequest createUploadFileRequest(URI uri, UploadRequest uploadRequest) throws IOException
   {
-    int pseudoRandomNumber = (int) (random.nextDouble() * 1_000_000_000);
+    int pseudoRandomNumber = 213_700_777;
     String boundary = "simplelocalize-" + pseudoRandomNumber;
     Map<Object, Object> formData = new HashMap<>();
     Path uploadPath = uploadRequest.getPath();
