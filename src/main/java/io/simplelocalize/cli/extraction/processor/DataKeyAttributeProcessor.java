72ee21433783e9ee23ad8a1dc6380c76619eaf05
Jakub Pomykała <jakub.pomykala@gmail.com>
2024-04-17 16:33:07 +0200

    Upgrade to Java 22

--- src/main/java/io/simplelocalize/cli/extraction/processor/DataKeyAttributeProcessor.java
+++ src/main/java/io/simplelocalize/cli/extraction/processor/DataKeyAttributeProcessor.java
@@ -2,13 +2,12 @@ package io.simplelocalize.cli.extraction.processor;
 
 import io.simplelocalize.cli.extraction.ExtractionResult;
 import io.simplelocalize.cli.extraction.files.BaseExtensionFilesFinder;
-import io.simplelocalize.cli.extraction.keys.DefaultKeysExtractor;
+import io.simplelocalize.cli.io.FileContentReader;
+import org.jsoup.Jsoup;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 public class DataKeyAttributeProcessor implements ExtractionProcessor
 {
@@ -21,11 +20,28 @@ public class DataKeyAttributeProcessor implements ExtractionProcessor
     List<ExtractionResult> output = new ArrayList<>();
     for (Path file : foundFiles)
     {
-      Set<String> batchKeys = DefaultKeysExtractor.extractValuesByPattern(file, Pattern.compile("(?<=data-i18n-key=[\"|'])(.*?)(?=[\"|'])"));
-      output.addAll(ExtractionResult.fromCollection(batchKeys, file));
+      output.addAll(extractTranslationsFromFile(file));
     }
     return output;
   }
+
+  private List<ExtractionResult> extractTranslationsFromFile(Path file)
+  {
+    String fileContent = FileContentReader.tryReadContent(file);
+    List<ExtractionResult> results = new ArrayList<>();
+
+    Jsoup.parse(fileContent).select("[data-i18n-key]").forEach(element -> {
+      String key = element.attr("data-i18n-key");
+      String text = element.text();
+      results.add(ExtractionResult.builder()
+              .key(key)
+              .translation(text)
+              .filePath(file)
+              .build());
+    });
+    return results;
+  }
+
 
   @Override
   public String getExtractTypeSupport()
