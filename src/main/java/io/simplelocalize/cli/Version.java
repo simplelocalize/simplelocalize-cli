b43a45b080477caa39b454738a9eafd198112e4b
Jakub Pomykała <jakub.pomykala@gmail.com>
2024-04-17 20:10:50 +0200

    Change version number

--- src/main/java/io/simplelocalize/cli/Version.java
+++ src/main/java/io/simplelocalize/cli/Version.java
@@ -3,7 +3,7 @@ package io.simplelocalize.cli;
 public class Version
 {
 
-  public static final String NUMBER = "2.6.0-beta";
+  public static final String NUMBER = "2.6.0";
 
   private Version()
   {
