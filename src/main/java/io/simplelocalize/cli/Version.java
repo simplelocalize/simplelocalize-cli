8360f6544b50347441f6421e54d743563951174d
Jakub Pomykała <jakub.pomykala@gmail.com>
2026-07-24 21:30:07 +0200

    Bump version

--- src/main/java/io/simplelocalize/cli/Version.java
+++ src/main/java/io/simplelocalize/cli/Version.java
@@ -3,7 +3,7 @@ package io.simplelocalize.cli;
 public class Version
 {
 
-  public static final String NUMBER = "2.11.1";
+  public static final String NUMBER = "2.11.2";
 
   private Version()
   {
